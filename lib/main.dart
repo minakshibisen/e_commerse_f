0f1fb2e467fa7299ea0344c3d599df7570ef9c1a
minakshi <minakshibisen04@gmail.com>
2024-12-09 18:28:51 +0530

    Dependancy

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerse_f/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
+        fontFamily: GoogleFonts.workSans().fontFamily,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
